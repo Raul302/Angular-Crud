7b1d73fc4c84b40815b10374e939a7d5a7c6f18a
Raul302 <Luis_1547@hotmail.com>
2020-02-13 23:19:39 -0600

    crud employees

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crud';
+
+  selectedEmployee: Employee = new Employee();
+  employeeArray: Employee[] =[
+    {id:1, name: "Raul", country: "mejico"},
+    {id:2, name: "Alberto", country: "Hondureño"},
+    {id:3, name: "Luis", country: "Guatemalteco"}
+  ];
+
+  addOrEdit()
+  {
+    if(this.selectedEmployee.id===0)
+    {
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+
+   this.selectedEmployee = new Employee();
+  }
+  openForEdit(employee: Employee)
+  {
+   this.selectedEmployee = employee;
+  }
+  delete()
+  {
+    if(confirm('Estas seguro que deseas eliminarlo?')){
+      this.employeeArray=this.employeeArray.filter(x=>x!=this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+
+  }
 }
